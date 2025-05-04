fda5ab8df4dffc9dcf159307e50ab040c76300fe
kamkow1 <kamkow256@gmail.com>
2025-05-04 20:12:31 +0200

    rewrite

--- qbel.go
+++ qbel.go
@@ -1,199 +1,164 @@
 package main
 
 import (
-	"flag"
-	"log"
+	"fmt"
 	"os"
-	"syscall"
-    "fmt"
 	"os/exec"
 	"path/filepath"
-    "crypto/rand"
+	"syscall"
 )
 
-type Options struct {
-    internal_command string
-    internal_uuid string
-    root string
-    shell string
-    mkimage_script string
-}
+var (
+	// ContainerRootfsPath : Path of the directory which to be used as root filesystem in container.
+	// You should have `rootfs` directory which contains a small linux file system.
+	// You can use BusyBox or a small Alpine linux image for that.
+	// Check https://wiki.alpinelinux.org/wiki/Installing_Alpine_Linux_in_a_chroot
+	ContainerRootfsPath = "./rootfs"
 
-var options Options
-var uuid string
-var qbelDir string
+	// OldRootPath : Path that old rootfilesystem to be moved in container.
+	OldRootPath = ".old_root"
+
+	// ContainerHostName : The name to be used as hostname in container
+	ContainerHostName = "container001"
+)
 
 func init() {
-    flag.StringVar(&options.root, "root", "", "Root FS mount path")
-    flag.StringVar(&options.shell, "shell", "", "Path to shell app")
-    flag.StringVar(&options.internal_command, "_ic", "create", "For internal usage")
-    flag.StringVar(&options.internal_uuid, "_iuuid", "", "For internal usage")
-    flag.StringVar(&options.mkimage_script, "mkimage", "", "Path to shell script which creates the image")
-    flag.Parse()
-    
-    uuid = pseudo_uuid()
-}
-
-func pseudo_uuid() (uuid1 string) {
-
-    b := make([]byte, 16)
-    _, err := rand.Read(b)
-    if err != nil {
-        fmt.Println("Error: ", err)
-        return
+    rootfs, ok := os.LookupEnv("QBEL_ROOTFS")
+    if ok {
+        ContainerRootfsPath = rootfs
     }
 
-    uuid1 = fmt.Sprintf("%04X-%04X-%04X-%04X-%04X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+    mkimage_script, ok := os.LookupEnv("QBEL_MKIMAGE")
+    if ok && mkimage_script != "" {
+        cmd := exec.Command(mkimage_script)
+        cmd.Dir = ContainerRootfsPath
+        cmd.Stdin = os.Stdin
+        cmd.Stdout = os.Stdout
+        cmd.Stderr = os.Stderr
+        if err := cmd.Run(); err != nil {
+            fmt.Println("ERROR", err)
+        }
 
-    return
-}
-
-func createQbel(root, shell, mkimage string) {
-    log.Println("Creating container...")
-
-    qbelDir = filepath.Join(root, "/.qbel-" + uuid)
-    
-    if err := os.MkdirAll(qbelDir, 0755); err != nil {
-        log.Fatalln("could not create directories", err)
-        os.Exit(1)
+        os.Setenv("QBEL_MKIMAGE", "")
     }
-
-    mkimage_path, err := filepath.Abs(mkimage)
-    if err != nil {
-        os.Exit(1)
-    }
-
-    exec.Command("cp", mkimage_path, filepath.Join(root, ".qbel-" + uuid)).Run()
-    
-    if err := runMkimageScript(filepath.Join(root, ".qbel-" + uuid, filepath.Base(mkimage)), root); err != nil {
-        log.Fatalln("Could not run mkimage script", err)
-        os.Exit(1)
-    }
-    
-    args := []string{
-        "-root=" + root,
-        "-shell=" + shell,
-        "-_ic=" + "shell",
-        "-mkimage=" + mkimage,
-        "-_iuuid=" + uuid,
-    }
-    cmd := exec.Command("/proc/self/exe", args...)
-    cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-
-    var flags uintptr
-    flags = syscall.CLONE_NEWNS  | syscall.CLONE_NEWUTS  |
-            syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID  |
-            syscall.CLONE_NEWNET | syscall.CLONE_NEWUSER
-
-    cmd.SysProcAttr = &syscall.SysProcAttr{
-        Cloneflags: flags,
-        UidMappings: []syscall.SysProcIDMap{
-            { ContainerID: 0, HostID: os.Getuid(), Size: 1 },
-        },
-        GidMappings: []syscall.SysProcIDMap{
-            { ContainerID: 0, HostID: os.Getuid(), Size: 1 },
-        },
-    }
-
-    if err := cmd.Run(); err != nil {
-        log.Fatalln("Error running container", err)
-        os.Exit(1)
-    }
-}
-
-
-func launchShell(root, shell, mkimage string) {
-    qbelDir = filepath.Join(root, "/.qbel-" + uuid)
-
-    log.Println("Launching shell", shell)
-    
-    err := syscall.Sethostname([]byte("qbel-"+uuid))
-    if err != nil {
-        log.Fatalln("Could not set hostname")
-        os.Exit(1)
-    }
-
-    err = pivotRoot(qbelDir, mkimage)
-    if err != nil {
-        log.Fatalln("Could not pivot root", err)
-        os.Exit(1)
-    }
-
-    cmd := exec.Command(shell)
-    cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-
-    if err = cmd.Run(); err != nil {
-        log.Fatalln("Running shell failed", err)
-        os.Exit(1)
-    }
-    log.Println("Quitting shell")
-}
-
-func pivotRoot(root, mkimage string) error {
-    if err := syscall.Mount(root, root, "", syscall.MS_BIND | syscall.MS_REC, ""); err != nil {
-        log.Fatalln("Could not mount", err)
-        return err
-    }
-    
-    
-    if err := syscall.PivotRoot(root, qbelDir); err != nil {
-        log.Fatalln("syscall pivot_root failed", err)
-        return err
-    }
-
-    if err := os.Chdir("/"); err != nil {
-        log.Fatalln("Could not chdir", err)
-        return err
-    }
-    
-    if err := syscall.Unmount("/", syscall.MNT_DETACH); err != nil {
-        log.Fatalln("Could not unmount", err)
-        return err
-    }
-
-    // if err := os.RemoveAll("/"); err != nil {
-    //     log.Fatalln("Could not remove directories", err)
-    //     return err
-    // }
-
-    return nil
-}
-
-func runMkimageScript(script_path, root string) error {
-    cmd := exec.Command(script_path)
-    cmd.Dir = filepath.Join(root, ".qbel-" + uuid)
-    cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-
-    return cmd.Run()
 }
 
 func main() {
-    if options.root == ""  ||
-       options.shell == "" ||
-       options.mkimage_script == "" {
-        flag.PrintDefaults()
-        os.Exit(1)
-    }
+	switch os.Args[1] {
+	case "run":
+		selfExec()
+	case "spawner":
+		spawner()
+	default:
+		panic("Unknown command")
+	}
+}
 
-    if options.internal_uuid != "" {
-        uuid = options.internal_uuid
-    }
+// Set the hostname of container
+func setHostName(hostName string) {
+	must(syscall.Sethostname([]byte(hostName)))
+}
 
-    if options.internal_command == "create" {
-        createQbel(options.root, options.shell, options.mkimage_script)
-        return
-    }
+// Make necessary mounts for container such as /proc.
+func mounts(newRoot string) {
+	procDict := filepath.Join(newRoot, "/proc")
+	_ = os.Mkdir(procDict, 0777)
+	must(syscall.Mount(newRoot, procDict, "proc", uintptr(0), ""))
+}
 
-    if options.internal_command == "shell" {
-        launchShell(options.root, options.shell, options.mkimage_script)
-        return
-    }
+// Prepare root fielsystem for container.
+func prepareRootfs(newRoot string) {
+	/*
+		From Linux man:
+		int pivot_root(const char *new_root, const char *put_old);
+			...
+			pivot_root() moves the root file system of the calling process to the directory put_old and makes
+			new_root the new root file system of the calling process.
 
-    flag.PrintDefaults()
+			...
+				The following restrictions apply to new_root and put_old:
+				-  They must be directories.
+				-  new_root and put_old must not be on the same filesystem as the current root.
+				-  put_old must be underneath new_root, that is, adding a nonzero number
+					of /.. to the string pointed to by put_old must yield the same directory as new_root.
+				-  No other filesystem may be mounted on put_old.
+	*/
+
+	// Since `new_root and put_old must not be on the same filesystem as the current root.`, we need to mount newroot.
+	must(syscall.Mount(newRoot, newRoot, "", syscall.MS_BIND|syscall.MS_REC, ""))
+
+	// Create `{newRoot}/.oldroot` for old root filesystem.
+	putOld := filepath.Join(newRoot, OldRootPath)
+	_ = os.Mkdir(putOld, 0777)
+
+	// This is related to the systemd mounts. We should change the roto mount to private before pivotting.
+	must(syscall.Mount("", "/", "", syscall.MS_REC|syscall.MS_PRIVATE, ""))
+
+	// Move the root filesystem to new filesystem
+	must(syscall.PivotRoot(newRoot, putOld))
+
+	// Change working directory to /
+	must(syscall.Chdir("/"))
+
+	// Unmount old filesystem which is now under /.old_root
+	must(syscall.Unmount(OldRootPath, syscall.MNT_DETACH))
+
+	// Remove temporary old filesystem
+	must(os.RemoveAll(OldRootPath))
+}
+
+func selfExec() {
+	// For some security reasons we should set hostname before running the actual container program. So we need a middle process
+	// that is cloned with new namespaces and this process should change hostname, prepare filesystem and then exec the container program.
+	cmd := exec.Command("/proc/self/exe", append([]string{"spawner"}, os.Args[2:]...)...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS |
+			syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNS |
+			syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getuid(),
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getgid(),
+				Size:        1,
+			},
+		},
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("ERROR", err)
+		os.Exit(1)
+	}
+}
+
+func spawner() {
+	mounts(ContainerRootfsPath)
+	prepareRootfs(ContainerRootfsPath)
+	setHostName(ContainerHostName)
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("ERROR", err)
+		os.Exit(1)
+	}
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
